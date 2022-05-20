7aab16b656b35ad4186452469b832a7fd81e6758
nyuta <nyuta.main@gmail.com>
2022-05-20 17:38:26 +0900

    ✨ feature tailwindcss

--- frontend/app/src/store/Todo/mutations.ts
+++ frontend/app/src/store/Todo/mutations.ts
@@ -1,11 +1,12 @@
 import { MutationTree } from 'vuex'
-import { TodosState, Todo } from '../types'
+import { TodosState } from '../types'
+import { Todo } from '@/types/todo'
 
 const mutations: MutationTree<TodosState> = {
   add: (state, todo: Todo) => {
     state.todos.push(todo)
   },
-  remove: (state, id: string) => {
+  remove: (state, id: number) => {
     state.todos = state.todos.filter((e: Todo) => e.id !== id)
   },
 }
