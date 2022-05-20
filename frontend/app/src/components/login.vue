8d2a61c7dc6dd2bac24b238cac10dc4d7940e07e
nyuta <nyuta.main@gmail.com>
2022-05-20 09:27:05 +0900

    ✨ feature login component

---
+++ frontend/app/src/components/login.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="login">
+<h1>This is loginPage</h1>
+<label>メールアドレス</label>
+<input type="text">
+<br>
+
+<label>password</label>
+<input type="password">
+</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+    data:{
+        "mail":""
+    },
+    components:{
+
+    }
+})
+export default class Login extends Vue {}
+</script>
