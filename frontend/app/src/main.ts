7cf94c7236415b622d34041c7fa42d4ffe8dc410
nyuta <nyuta.main@gmail.com>
2022-05-26 07:24:30 +0900

    ✨ introduce fontawesome and remove vue-awesome

--- frontend/app/src/main.ts
+++ frontend/app/src/main.ts
@@ -3,11 +3,21 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './index.css'
-import 'vue-awesome/icons'
-import Icon from 'vue-awesome/components/Icon'
 
-// globally (in your main .js file)
-Vue.component('v-icon', Icon)
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* add icons to the library */
+library.add(faUserSecret)
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 new Vue({
   router,
